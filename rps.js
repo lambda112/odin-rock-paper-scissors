1a142ddf92ca5c808b96d51e7498d3a38a51145e
Kamran Azim <Kamazim121212@gmail.com>
2026-08-26 07:15:15 +0100

    Get User Choice
    - Use prompt to get user input
    - Clean data using toLowerCase()
    - Check which attack user picked using if statements

--- rps.js
+++ rps.js
@@ -4,18 +4,43 @@
 function getComputerChoice(num = Math.floor(Math.random() * 3)) {
     // if parameter == 0 choose rock
     if (num == 0) {
-        return "Rock"
+        return "Rock";
     }
 
     // else if parameter == 1 choose scissors
     else if (num == 1) {
-        return "Scissors"
+        return "Scissors";
     }
 
     // else choose paper
     else {
-        return "Paper"
+        return "Paper";
     }
 }
 
-console.log(getComputerChoice())
+// Create function getHumanChoice
+function getHumanChoice() {
+    // While prompt does not equal rock, paper or scissors loop
+    while (true) {
+
+        // Get user input
+        userChoice = prompt("Choose Between Rock, Paper and Scissors.").toLowerCase()
+
+        // if input equals rock return rock
+        if (userChoice == "rock") {
+            return "Rock";
+        }
+
+        // else if input equals scissors return scissors
+        else if (userChoice == "scissors") {
+            return "Scissors";
+        }
+
+        // else if input equals paper return paper
+        else if (userChoice == "paper"){
+            return "Paper";
+        }
+    }
+}
+
+console.log(getHumanChoice())
